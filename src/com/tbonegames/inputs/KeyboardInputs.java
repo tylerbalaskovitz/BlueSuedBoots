aabe236507e93587b6fe51b1d8300290b085ca64
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-03-28 21:52:01 -0400

    Added mouse movements as well as keyboard movements

--- src/com/tbonegames/inputs/KeyboardInputs.java
+++ src/com/tbonegames/inputs/KeyboardInputs.java
@@ -3,9 +3,15 @@ package com.tbonegames.inputs;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import com.tbonegames.main.GamePanel;
+
 public class KeyboardInputs implements KeyListener{
 //ctrl + 1 is to add unimplemented methods easily without having to click anything
-	
+
+	private GamePanel gamePanel;
+	public KeyboardInputs(GamePanel gamePanel) {
+		this.gamePanel = gamePanel;
+	}
 	
 	@Override
 	public void keyTyped(KeyEvent e) {
@@ -15,10 +21,10 @@ public class KeyboardInputs implements KeyListener{
 	@Override
 	public void keyPressed(KeyEvent e) {
 		switch(e.getKeyCode()) {
-		case KeyEvent.VK_W:System.out.println("W was pressed");break;
-		case KeyEvent.VK_A:System.out.println("A was pressed");break;
-		case KeyEvent.VK_S:System.out.println("S was pressed");break;
-		case KeyEvent.VK_D:System.out.println("D was pressed");break;
+		case KeyEvent.VK_W:gamePanel.changeYDelta(-5);break;
+		case KeyEvent.VK_A:gamePanel.changeXDelta(-5);break;
+		case KeyEvent.VK_S:gamePanel.changeYDelta(5);break;
+		case KeyEvent.VK_D:gamePanel.changeXDelta(5);;break;
 			
 		}
 		
---
+++ src/com/tbonegames/inputs/MouseInputs.java
@@ -0,0 +1,52 @@
+
+package com.tbonegames.inputs;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import com.tbonegames.main.GamePanel;
+
+public class MouseInputs implements MouseListener, MouseMotionListener{
+	private GamePanel gamePanel;
+	public MouseInputs(GamePanel gamePanel) {
+		this.gamePanel = gamePanel;
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		gamePanel.setRectPos(e.getX(), e.getY());
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		System.out.println("The mouse was clicked!");
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
