beebb12ac651008f3cf2eb7526ce8f209a6be5c2
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-03-29 21:29:37 -0400

    Finished the game loop

--- src/com/tbonegames/main/GamePanel.java
+++ src/com/tbonegames/main/GamePanel.java
@@ -1,6 +1,8 @@
 package com.tbonegames.main;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -12,12 +14,14 @@ public class GamePanel extends JPanel{
 	private MouseInputs mouseInputs;
 	private float xDelta = 100, yDelta = 100;
 	private float xDir = .001f, yDir = .001f;
+	private Color color;
 	private int frames = 0;
 	private long lastCheck = 0;
+	private Random random;
 	
 	//the panel is the picture
 	public GamePanel() {
-		
+		random = new Random();
 		mouseInputs = new MouseInputs(this);
 		
 		addKeyListener(new KeyboardInputs(this));
@@ -45,6 +49,7 @@ public class GamePanel extends JPanel{
 		super.paintComponent(g);
 		
 		updateRectangle();
+		g.setColor(color);
 		
 		g.fillRect((int)xDelta, (int)yDelta, 200, 50);
 		frames++;
@@ -53,19 +58,29 @@ public class GamePanel extends JPanel{
 			System.out.println("FPS: " + frames);
 			frames = 0;
 		}
-		repaint();
 	}
 	
 	public void updateRectangle() {
 		xDelta+= xDir;
 		if (xDelta>400||xDelta<0) {
 			xDir*=-1;
+			color = getRndColor();
 		}
 		
 		yDelta+= yDir;
 		if (yDelta > 400||yDelta<0) {
 			yDir*=-1;
+			color = getRndColor();
 		}
+	}
+	
+	private Color getRndColor() {
+		int r=random.nextInt(255);
+		int g=random.nextInt(255);
+		int b=random.nextInt(255);
+		
+		return new Color(r,g,b);
+		
 	}
 	
 }
