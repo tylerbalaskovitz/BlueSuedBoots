12e448835a50713c95a7c4942cd0e45699f82058
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-03-29 21:06:22 -0400

    Begun to add a game look and experimenting with different loop types

--- src/com/tbonegames/main/GamePanel.java
+++ src/com/tbonegames/main/GamePanel.java
@@ -10,7 +10,10 @@ import com.tbonegames.inputs.MouseInputs;
 public class GamePanel extends JPanel{
 
 	private MouseInputs mouseInputs;
-	private int xDelta = 100, yDelta = 100;
+	private float xDelta = 100, yDelta = 100;
+	private float xDir = .001f, yDir = .001f;
+	private int frames = 0;
+	private long lastCheck = 0;
 	
 	//the panel is the picture
 	public GamePanel() {
@@ -24,19 +27,16 @@ public class GamePanel extends JPanel{
 	
 	public void changeXDelta(int value) {
 		this.xDelta += value;
-		repaint();
 		
 	}
 	
 	public void changeYDelta(int value) {
 		this.yDelta += value;
-		repaint();
 	}
 	
 	public void setRectPos(int x, int y) {
 		this.xDelta = x;
 		this.yDelta = y;
-		repaint();
 	}
 	
 	//This gets called whenever we press the play button.
@@ -44,7 +44,28 @@ public class GamePanel extends JPanel{
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		g.fillRect(xDelta, yDelta, 200, 50);
+		updateRectangle();
+		
+		g.fillRect((int)xDelta, (int)yDelta, 200, 50);
+		frames++;
+		if(System.currentTimeMillis() - lastCheck>= 1000) {
+			lastCheck = System.currentTimeMillis();
+			System.out.println("FPS: " + frames);
+			frames = 0;
+		}
+		repaint();
+	}
+	
+	public void updateRectangle() {
+		xDelta+= xDir;
+		if (xDelta>400||xDelta<0) {
+			xDir*=-1;
+		}
+		
+		yDelta+= yDir;
+		if (yDelta > 400||yDelta<0) {
+			yDir*=-1;
+		}
 	}
 	
 }
