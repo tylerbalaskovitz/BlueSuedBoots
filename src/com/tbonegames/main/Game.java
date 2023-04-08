c65d2e53a451eb93f126e8b125c38a732c306469
Tyler <tylerbalaskovitz@yahoo.com>
2023-04-08 13:51:52 -0400

    Adding a new loop of that allows for timings to be saved so the game doesn't lose information when going through updates. So, separating the FPS loops and the update loops for better game optimization.

--- src/com/tbonegames/main/Game.java
+++ src/com/tbonegames/main/Game.java
@@ -6,6 +6,7 @@ public class Game implements Runnable{
 	private GameWindow gameWindow;
 	private Thread gameThread;
 	private final int FPS_SET = 120;
+	private final int UPS_SET = 200;
 	
 	public Game() {
 		
@@ -25,13 +26,38 @@ public class Game implements Runnable{
 	public void run() {
 
 		double timePerFrame = 1000000000.0/FPS_SET;
+		
+		//time of the frequency
+		double timePerUpdate = 1000000000.0/UPS_SET;
 		long lastFrame = System.nanoTime();
 		long now = System.nanoTime();
+		
+		long previousTime = System.nanoTime();
+		
+		int updates = 0;
 		int frames = 0;
 		long lastCheck = System.currentTimeMillis();
+		
+		double deltaU = 0;
 		while(true) {
 
 			now = System.nanoTime();
+			
+			//currently this game loop slowly loses time over a longer period of time due to the fact that 
+			//now - lastFrame can exceed the timePerFrame double, and creating code inefficies that need to be
+			//corrected. 
+			long currentTime = System.nanoTime();
+			
+			deltaU += (currentTime - previousTime) / timePerUpdate;
+			
+			//this allows the variable to be held and controlled so there isn't any data loss, and allows the next update
+			// to come sooner creating an offset in the update loop regarding deltaU.
+			if (deltaU >= 1) {
+				//update();
+				updates++;
+					deltaU--;
+			}
+			
 		if(now - lastFrame >= timePerFrame) {
 			
 			gamePanel.repaint();
