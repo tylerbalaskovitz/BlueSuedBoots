2adcc6a511789482e2b78177eaffab605e5e7c96
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-04-29 00:15:32 -0400

    Created the HelpMethods class that contains static methods such as the isSolid and the canMoveHere methods. Both of these are used to check a particular space within the game to determine whether or not a place is solid by looking at the x and y coordinates, and return whether it is movable. That is ran through the can move Here method of the same parametmers to further check whether or not you can move there

---
+++ src/utils/HelpMethods.java
@@ -0,0 +1,44 @@
+package utils;
+
+import com.tbonegames.main.Game;
+
+//static methods to take in data and return values
+public class HelpMethods {
+	
+	public static boolean canMoveHere(float x, float y, int width, int height, int [][] levelData) {
+		
+		if (!isSolid(x, y, levelData)) {
+			if (!isSolid(x + width, y + height, levelData)) {
+				if (!isSolid(x+width, y , levelData)) {
+					if (!isSolid(x, y+height, levelData)) {
+						return true;
+					}
+				}
+			}
+		} return false;
+		
+		
+	}
+	
+	private static boolean isSolid(float x, float y, int[][] levelData) {
+		if (x < 0 || x >= Game.GAME_WIDTH) {
+			return true;
+		}
+		if (y < 0 || y >= Game.GAME_HEIGHT) {
+			return true;
+		}
+		
+		float xIndex = x / Game.TILES_SIZE;
+		float yIndex = y / Game.TILES_SIZE;
+		
+		int value = levelData[(int)yIndex][(int)xIndex];
+		
+		if (value >= 48 || value < 0 || value != 11) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+
+}
