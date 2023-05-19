a55cad3a4bbe6071b24e5bc02e6a8c8f18565735
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-05-19 18:48:13 -0400

    Pushing a bit more code that includes the loadImages() method that includes the image for loading the menu, etc etc

--- src/ui/MenuButton.java
+++ src/ui/MenuButton.java
@@ -1,9 +1,26 @@
 package ui;
 
+import java.awt.image.BufferedImage;
+
+import gamestates.Gamestate;
+
 public class MenuButton {
 
-	public MenuButton(int xPos, int yPos, int rowIndex) {
-		
+	private int xPos, yPos, rowIndex;
+	private Gamestate state;
+	private BufferedImage[] images;
+	
+	
+	public MenuButton(int xPos, int yPos, int rowIndex, Gamestate state) {
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.rowIndex = rowIndex;
+		this.state = state;
+		loadImages();
+	}
+	
+	private void loadImages() {
+		images = new BufferedImage[3];
 	}
 	
 }
